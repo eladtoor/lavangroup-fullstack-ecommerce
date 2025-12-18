320c5ab5aa56bb9bad999b83c1a9b09b07360ae6
eladtoor <eladtoordev@gmail.com>
2025-12-18 18:12:33 +0200

    feat: add Google Analytics 4 (GA4) integration with measurement ID G-W93SPNXDKS

--- web/src/components/GoogleTagManager.tsx
+++ web/src/components/GoogleTagManager.tsx
@@ -3,6 +3,7 @@
 import Script from 'next/script';
 
 const GTM_ID = 'GTM-WCGRTXK2';
+const GA4_MEASUREMENT_ID = 'G-W93SPNXDKS';
 
 export default function GoogleTagManager() {
   return (
@@ -18,6 +19,24 @@ export default function GoogleTagManager() {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+      
+      {/* Google Analytics 4 (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA4_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA4_MEASUREMENT_ID}');
           `,
         }}
       />
